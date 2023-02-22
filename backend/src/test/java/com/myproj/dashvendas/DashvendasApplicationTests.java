bb2badd995c9ec2bcc932e0c56b3acfbc8c2ae38
Vitor Alves <vitorcarneiro778@gmail.com>
2023-02-22 19:34:38 -0300

    First Commit

---
+++ backend/src/test/java/com/myproj/dashvendas/DashvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproj.dashvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
