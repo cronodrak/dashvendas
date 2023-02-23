c70593e14048f3b85724632c2e0d23c510fbbc7c
Vitor Alves <vitorcarneiro778@gmail.com>
2023-02-23 19:54:53 -0300

    header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import Navbar from "components/NavBar";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo</h1>
+      </div>
+    </>
   );
 }
 
