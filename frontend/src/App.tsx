069bd3ce905e12b14d7aa47d5b7d83821a1f9345
Vitor Alves <vitorcarneiro778@gmail.com>
2023-02-24 19:37:10 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
 import Navbar from "components/NavBar";
 
 function App() {
@@ -6,7 +8,9 @@ function App() {
       <Navbar />
       <div className="container">
         <h1 className="text-primary">Olá mundo</h1>
+        <DataTable />
       </div>
+      <Footer />
     </>
   );
 }
