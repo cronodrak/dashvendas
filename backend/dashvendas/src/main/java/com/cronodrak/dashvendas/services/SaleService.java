f67d0a05829f36cdf93412714f6af2a109a4fd4e
Vitor Alves <vitorcarneiro778@gmail.com>
2023-03-03 19:22:16 -0300

    Pagination

--- backend/dashvendas/src/main/java/com/cronodrak/dashvendas/services/SaleService.java
+++ backend/dashvendas/src/main/java/com/cronodrak/dashvendas/services/SaleService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cronodrak.dashvendas.dto.SaleDTO;
 import com.cronodrak.dashvendas.entities.Sale;
 import com.cronodrak.dashvendas.respositories.SaleRepository;
+import com.cronodrak.dashvendas.respositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -15,7 +17,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
 		Page<Sale> result = saleRepository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
